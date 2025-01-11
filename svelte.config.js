8d1137a9624ddfed7e2e62af33e6498ef2ebea4f
Frank Rørtvedt <29666084+frankrtv@users.noreply.github.com>
2025-01-12 00:14:41 +0100

    fix: update svelte config for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/viteswelt' : ''
+			base: process.env.NODE_ENV === 'production' ? '/viteswelt' : '',
+			relative: false
 		}
 	},
 	preprocess: vitePreprocess()
